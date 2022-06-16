eb78741e6c9ad10f61d0bb89dae378da26830b53
CodieDigital <tiago.leanwork@gmail.com>
2022-06-16 17:02:55 -0300

    fix: bug title

--- components/data/title/styles.ts
+++ components/data/title/styles.ts
@@ -6,7 +6,9 @@ interface TitleStyleProps {
     max: number | "45" | "60" | "100";
     variable: number;
     min: number;
+
   } 
+  marginSubTitle?: number;
 }
 
 export const Title = styled.div<TitleStyleProps>`
@@ -19,9 +21,14 @@ export const Title = styled.div<TitleStyleProps>`
         : "var(--text-primary-color-dark)"};
   }
 
+  span {
+    margin-bottom: ${props => props.marginSubTitle};
+  }
+
   h1,
   h2 {
     line-height: 0.9;
     margin: 0;
+    text-transform: lowercase;
   }
 `;
